430ad3c368a0e2fafa1ad8ffe128e6abd663d736
ray <henrayluo@gmail.com>
2018-12-04 18:33:08 +0800

    格式化代码。

--- WXDemo/mmtls/ClientHello.m
+++ WXDemo/mmtls/ClientHello.m
@@ -11,7 +11,7 @@
 #import "NSData+GenRandomData.h"
 #import "ECDH.h"
 
-@interface ClientHello()
+@interface ClientHello ()
 
 @property (nonatomic, strong) NSData *clientRandom;
 
@@ -27,62 +27,70 @@
 
 @implementation ClientHello
 
-- (instancetype)init {
+- (instancetype)init
+{
     self = [super init];
-    if (self) {
+    if (self)
+    {
         _clientRandom = [NSData GenRandomDataWithSize:32];
         NSData *pubkey1;
         NSData *prikey1;
         BOOL ret1 = [ECDH GenEcdhWithNid:415 priKey:&prikey1 pubKeyData:&pubkey1];
-        if (ret1) {
+        if (ret1)
+        {
             _prikey1 = prikey1;
             _pubkey1 = pubkey1;
         }
-        
+
         NSData *pubkey2;
         NSData *prikey2;
         BOOL ret2 = [ECDH GenEcdhWithNid:415 priKey:&prikey2 pubKeyData:&pubkey2];
-        if (ret2) {
+        if (ret2)
+        {
             _prikey2 = prikey2;
             _pubkey2 = pubkey2;
         }
     }
-    
+
     return self;
 }
 
-- (NSData *)CreateClientHello {
-    if (!_clientHelloData) {
+- (NSData *)CreateClientHello
+{
+    if (!_clientHelloData)
+    {
         NSMutableData *clientHelloData = [[NSData dataWithHexString:@"16F10300D4"] mutableCopy]; //mmtls head [16F10300D4: 0xD4为后面包长度]
-        [clientHelloData appendData:[NSData dataWithHexString:@"000000D00103F101C02B"]];//fix
-        [clientHelloData appendData:_clientRandom]; //client random
-        
+        [clientHelloData appendData:[NSData dataWithHexString:@"000000D00103F101C02B"]];         //fix
+        [clientHelloData appendData:_clientRandom];                                              //client random
+
         NSUInteger timeStamp = [[NSDate date] timeIntervalSince1970];
-        [clientHelloData appendData:[NSData packInt32:(int32_t)timeStamp flip:NO]]; //time
+        [clientHelloData appendData:[NSData packInt32:(int32_t) timeStamp flip:NO]];         //time
         [clientHelloData appendData:[NSData dataWithHexString:@"000000A2010000009D001002"]]; //fix
-        
+
         [clientHelloData appendData:[NSData dataWithHexString:@"00000047000000010041"]]; //fix
-        [clientHelloData appendData:_pubkey1]; //pubkey
-        
+        [clientHelloData appendData:_pubkey1];                                           //pubkey
+
         [clientHelloData appendData:[NSData dataWithHexString:@"00000047000000020041"]]; //fix
-        [clientHelloData appendData:_pubkey2]; //pubkey
-        
+        [clientHelloData appendData:_pubkey2];                                           //pubkey
+
         [clientHelloData appendData:[NSData dataWithHexString:@"00000001"]]; //fix
-        
+
         _clientHelloData = [clientHelloData copy];
-        
+
         DLog(@"Client Hello", _clientHelloData);
     }
-    
+
     return _clientHelloData;
 }
 
-- (NSData *)getHashPart {
+- (NSData *)getHashPart
+{
     NSData *header = [NSData dataWithHexString:@"16F10300D4"];
     return [_clientHelloData subdataWithRange:NSMakeRange([header length], 0xD4)];
 }
 
-- (NSData *)getLocal1stPrikey {
+- (NSData *)getLocal1stPrikey
+{
     return _prikey1;
 }
 
