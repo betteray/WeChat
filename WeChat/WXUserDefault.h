adb19fdfef086da4c0d717cdad74a848f1ff9e1b
ray <henrayluo@gmail.com>
2018-12-20 16:09:27 +0800

    整理代码。

--- WeChat/WXUserDefault.h
+++ WeChat/WXUserDefault.h
@@ -8,6 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface WXUserDefault : NSObject
 
 + (void)saveWXID:(NSString *)wxid;
@@ -22,5 +24,6 @@
 + (void)saveAlias:(NSString *)alias;
 + (NSString *)getAlias;
 
-
 @end
+
+NS_ASSUME_NONNULL_END
--- WeChat/WXUserDefault.m
+++ WeChat/WXUserDefault.m
@@ -8,45 +8,53 @@
 
 #import "WXUserDefault.h"
 
-#define WXID        @"WXID"
-#define UIN         @"UIN"
-#define ALIAS       @"ALIAS"
-#define NIKENAME    @"NIKENAME"
+#define WXID @"WXID"
+#define UIN @"UIN"
+#define ALIAS @"ALIAS"
+#define NIKENAME @"NIKENAME"
 #define CLIENTCHECKDATA @"CLIENTCHECKDATA"
 #define LONGLINKIP @"LONGLINKIP"
 #define SHORTLINKIP @"SHORTLINKIP"
 
 @implementation WXUserDefault
 
-+ (void)saveWXID:(NSString *)wxid {
++ (void)saveWXID:(NSString *)wxid
+{
     [[NSUserDefaults standardUserDefaults] setObject:wxid forKey:WXID];
 }
 
-+ (NSString *)getWXID {
++ (NSString *)getWXID
+{
     return [[NSUserDefaults standardUserDefaults] objectForKey:WXID];
 }
 
-+ (void)saveUIN:(NSInteger)uin {
++ (void)saveUIN:(NSInteger)uin
+{
     [[NSUserDefaults standardUserDefaults] setInteger:uin forKey:UIN];
 }
 
-+ (NSInteger)getUIN {
++ (NSInteger)getUIN
+{
     return [[NSUserDefaults standardUserDefaults] integerForKey:UIN];
 }
 
-+ (void)saveNikeName:(NSString *)nikename {
++ (void)saveNikeName:(NSString *)nikename
+{
     [[NSUserDefaults standardUserDefaults] setObject:nikename forKey:NIKENAME];
 }
 
-+ (NSString *)getNikeName {
++ (NSString *)getNikeName
+{
     return [[NSUserDefaults standardUserDefaults] objectForKey:NIKENAME];
 }
 
-+ (void)saveAlias:(NSString *)alias {
++ (void)saveAlias:(NSString *)alias
+{
     [[NSUserDefaults standardUserDefaults] setObject:alias forKey:ALIAS];
 }
 
-+ (NSString *)getAlias {
++ (NSString *)getAlias
+{
     return [[NSUserDefaults standardUserDefaults] objectForKey:ALIAS];
 }
 
