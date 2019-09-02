0159286b4975d5f249f5db2f0001eee0910126dd
ray <henrayluo@gmail.com>
2019-09-02 17:36:51 +0800

    gen ios deviceid.

--- WeChat/CUtility.m
+++ WeChat/CUtility.m
@@ -7,6 +7,8 @@
 //
 
 #import "CUtility.h"
+#import "FSOpenSSL.h"
+
 
 @implementation CUtility
 
@@ -22,6 +24,20 @@
     NSInteger buildVersion = [components[3] integerValue];
     
     return (uint32_t) ((majorVersion << 24) | (minorVersion << 16) | (patchVersion << 8) | buildVersion | 0x10000000);
+}
+
++ (NSString *)GetUUIDNew
+{
+    NSUUID *uuid = [[UIDevice currentDevice] identifierForVendor];
+    NSString *uuidString = [uuid UUIDString];
+    uuidString = [uuidString stringByReplacingOccurrencesOfString:@"-" withString:@""];
+    return [FSOpenSSL md5FromString:uuidString];
+}
+
++ (NSString *)GetDeviceID
+{
+    NSString *uuidNew = [self GetUUIDNew];
+    return [uuidNew stringByReplacingCharactersInRange:NSMakeRange(0, 2) withString:@"49"];
 }
 
 @end
