7fa731c665f3594e8dca6ed5f651dcd404f132b0
ray <henrayluo@gmail.com>
2020-01-02 19:32:09 +0800

    CHG: 算 ClientCheckConsistency 增加字段说明。

--- WeChat/Crypto/ClientCheckConsistencyUtil.m
+++ WeChat/Crypto/ClientCheckConsistencyUtil.m
@@ -29,6 +29,14 @@
 //    super.onCreate(savedInstanceState);
 //    setContentView(R.layout.activity_main);
 //
+//    // sysmsg.ClientCheckConsistency.clientcheck.fullpathfilename
+//    // sysmsg.ClientCheckConsistency.clientcheck.fileoffset
+//    // sysmsg.ClientCheckConsistency.clientcheck.checkbuffersize
+//    // sysmsg.ClientCheckConsistency.clientcheck.seq
+//    // 要算的dex的md5 string
+//    // dex 实际大小
+//    // 是否root： 0为非root，1为root。
+//    // 网络类型，计算方法z见下面java代码。
 //    String sign = sign(new Object[]{"@classes.dex", 0, 9999999, 536870912, "28c3bf8c8127800e6546d8de1a361dcf", 9481764, 0, 4});
 //    Log.d(TAG, String.format("onCreate: %s", sign)); //1962359c264a15f2edf38ae300e7ec0c
 //}
@@ -91,4 +99,38 @@
 //    return "";
 //}
 //
+
+
+//public static int GetNetworkType() {
+//    AppMethodBeat.i(93018);
+//    NetworkInfo activeNetworkInfo = ((ConnectivityManager) ah.getContext().getSystemService("connectivity")).getActiveNetworkInfo();
+//    if (activeNetworkInfo == null) {
+//        return 0;
+//    }
+//    int subtype = activeNetworkInfo.getSubtype();
+//    if (activeNetworkInfo.getType() == 1) {
+//        return 1;
+//    }
+//    switch (subtype) {
+//        case 1:
+//        case 2:
+//        case 7:
+//        case 11:
+//            return 2;
+//        case 3:
+//        case 4:
+//        case 5:
+//        case 6:
+//        case 12:
+//        case 17:
+//            return 3;
+//        case 13:
+//        case 14:
+//        case 15:
+//            return 4;
+//        default:
+//            return 0;
+//    }
+//}
+
 @end
