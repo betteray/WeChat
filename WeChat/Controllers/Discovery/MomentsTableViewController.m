851119ff9abeb8b41e16b9cdde025f995a63fa77
ray <henrayluo@gmail.com>
2020-05-26 16:55:29 +0800

    cdn upload sns with pics.

--- WeChat/Controllers/Discovery/MomentsTableViewController.m
+++ WeChat/Controllers/Discovery/MomentsTableViewController.m
@@ -24,26 +24,26 @@
 }
 
 - (IBAction)sendSnsPostRequest:(id)sender {
-//    NSMutableArray *ma = [NSMutableArray array];
-//    for (int i = 1; i < 4 ; i++) {
-//        NSString *pic = [[NSBundle mainBundle] pathForResource:[NSString stringWithFormat:@"pic_%d", i] ofType:@"jpg"];
-//        [ma addObject:pic];
-//    }
-//
-//    [[CdnLogic sharedInstance] startC2CUpload:ma success:^(NSArray *  _Nullable response) {
-//        LogVerbose(@"上传朋友圈图片 response： %@", response);
-//        if ([response count]) {
-//            [SnsPostService startSendSNSPost:response];
-//        }
-//    } failure:^(NSError * _Nonnull error) {
-//
-//    }];
+    NSMutableArray *ma = [NSMutableArray array];
+    for (int i = 1; i < 4 ; i++) {
+        NSString *pic = [[NSBundle mainBundle] pathForResource:[NSString stringWithFormat:@"pic_%d", i] ofType:@"jpg"];
+        [ma addObject:pic];
+    }
+
+    [[CdnLogic sharedInstance] startC2CUpload:ma success:^(NSArray *  _Nullable response) {
+        LogVerbose(@"上传朋友圈图片 response： %@", response);
+        if ([response count]) {
+            [SnsPostService startSendSNSPost:response];
+        }
+    } failure:^(NSError * _Nonnull error) {
+
+    }];
 //    NSString *pic = [[NSBundle mainBundle] pathForResource:@"pic_1" ofType:@"jpg"];
 //    [SnsUploadImageService SnsUpload:pic starPos:0];
     
-    [SnsPostService startSendSNSPost:@[@{@"filekey":@"",
-                                         @"fileurl": @"http://mmsns.qpic.cn/mmsns/PiajxSqBRaEJqiaOiaOs1mVlDOpllHDOjgYTQ2ibFukn8a8nOrJ3rmvW7lYytkGdDOicQ/0",
-                                         @"thumburl":@"http://mmsns.qpic.cn/mmsns/PiajxSqBRaEJqiaOiaOs1mVlDOpllHDOjgYTQ2ibFukn8a8nOrJ3rmvW7lYytkGdDOicQ/150"}]];
+//    [SnsPostService startSendSNSPost:@[@{@"filekey":@"",
+//                                         @"fileurl": @"http://mmsns.qpic.cn/mmsns/PiajxSqBRaEJqiaOiaOs1mVlDOpllHDOjgYTQ2ibFukn8a8nOrJ3rmvW7lYytkGdDOicQ/0",
+//                                         @"thumburl":@"http://mmsns.qpic.cn/mmsns/PiajxSqBRaEJqiaOiaOs1mVlDOpllHDOjgYTQ2ibFukn8a8nOrJ3rmvW7lYytkGdDOicQ/150"}]];
 }
 
 #pragma mark - Table view data source
